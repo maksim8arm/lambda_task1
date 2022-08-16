43e439cee1560b84b7ceb9b3d6e3df1ef5828386
Makism.Armenskiy <maxim.arm8@mail.ru>
2022-08-16 20:36:26 +0300

    add files

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.devide.apply(a, b);//деление на ноль, изменил логику лямбда выражения в devide
+
+        calc.println.accept(c);
+
+    }
+}
